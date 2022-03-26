d224bf5b881d725b3c8db8ff86a8f743a173210e
PlayerG9 <g9yout@gmail.com>
2022-03-26 18:15:16 +0100

    added file for generell css code

--- src/web-ui/src/App.jsx
+++ src/web-ui/src/App.jsx
@@ -1,13 +1,18 @@
 import './App.css'
+import './generell.css'
 import {
     BrowserRouter,
     Routes,
-    Route
+    Route,
+
+    Navigate
 } from 'react-router-dom'
 
 import NarBar from './routes/navbar'
 import HomePage from './routes/home'
-import Encryptor from './routes/encryptor'
+import Period2Number from './routes/period2number'
+import Number2Period from './routes/number2period'
+import TestPage, { isEnabled as testPageEnabled} from './routes/tests'
 import Page404NotFound from './routes/page404notfound'
 
 
@@ -17,7 +22,13 @@ export default function App() {
         <Routes>
             <Route path="/">
                 <Route index element={<HomePage/>}/>
-                <Route path="encryptor" element={<Encryptor/>}/>
+                <Route path="p2n" element={<Navigate to="period2number"/>}/>
+                <Route path="period2number" element={<Period2Number/>}/>
+                <Route path="n2p" element={<Navigate to="number2period"/>}/>
+                <Route path="number2period" element={<Number2Period/>}/>
+                {testPageEnabled &&
+                    <Route path="tests" element={<TestPage/>}/>
+                }
                 <Route path="*" element={<Page404NotFound/>}/>
             </Route>
         </Routes>
